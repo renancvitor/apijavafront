e78816ed21c49741fb2e9d44acb6e273a78c5aed
renancvitor <renan.vitor.cm@gmail.com>
2025-05-22 18:19:04 -0300

    Devolvendo s[eries no navegador

--- src/main/java/com/renan/javaspring/controller/SerieController.java
+++ src/main/java/com/renan/javaspring/controller/SerieController.java
@@ -1,13 +1,21 @@
 package com.renan.javaspring.controller;
 
+import com.renan.javaspring.model.Serie;
+import com.renan.javaspring.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui vão ser listadas as séries";
+    public List<Serie> obterSeries() {
+        return repository.findAll();
     }
 }
