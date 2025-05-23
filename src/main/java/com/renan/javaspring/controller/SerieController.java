ed034ae8d63dae0ec02bafbbe359564fad52b3f6
renancvitor <renan.vitor.cm@gmail.com>
2025-05-23 07:00:35 -0300

    Retornando os detalhes da série

--- src/main/java/com/renan/javaspring/controller/SerieController.java
+++ src/main/java/com/renan/javaspring/controller/SerieController.java
@@ -4,6 +4,7 @@ import com.renan.javaspring.dto.SerieDTO;
 import com.renan.javaspring.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +30,10 @@ public class SerieController {
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos() {
         return service.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return service.obterPorId(id);
     }
 }
--- src/main/java/com/renan/javaspring/service/SerieService.java
+++ src/main/java/com/renan/javaspring/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,5 +34,17 @@ public class SerieService {
 
     public List<SerieDTO> obterLancamentos() {
         return converteDados(repository.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repository.findById(id);
+
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(),
+                    s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(),
+                    s.getSinops());
+        }
+        return null;
     }
 }
